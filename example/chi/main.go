f5f966cb24266318f27bff3d2f7999241e5ff484
Tyler Pham <tyler@disruptiveart.com>
2016-01-20 08:51:09 -0500

    added admin route

--- example/chi/main.go
+++ example/chi/main.go
@@ -26,9 +26,17 @@ func main() {
 	// Limit to 5 requests globally.
 	r.Use(throttler.Limit(5))
 	r.Get("/*", handler)
-  
+
+	admin := chi.NewRouter()
+	// Limit to 2 requests for admin route
+	admin.Use(throttler.Limit(2))
+	admin.Get("/", handler)
+	
+	r.Mount("/admin/", admin)
+
 	fmt.Printf("Try running the following commands (in different terminal):\n\n")
 	fmt.Printf("for i in `seq 1 10`; do (curl 127.0.0.1:8000/ &); done\n\n")
+	fmt.Printf("for i in `seq 1 10`; do (curl 127.0.0.1:8000/admin/ &); done\n\n")
   
 	http.ListenAndServe(":8000", r)
 }
